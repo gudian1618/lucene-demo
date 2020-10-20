ae8e37de447d182a11a9892f0555c3d4522df2a0
gudian1618 <1761844211@qq.com>
2020-10-20 22:45:08 +0800

    Initial commit

---
+++ src/test/java/test/Test1.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Test;
+
+import java.io.File;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2020/10/20 10:38 下午
+ */
+
+public class Test1 {
+    @Test
+    public void test1() {
+        File path = new File("src/test/java/test");
+    }
+}
